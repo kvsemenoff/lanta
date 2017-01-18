fdb166de5dff56ec6f799739f59ac4235a8171a0
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-18 17:41:30 +0300

    d

---
+++ lanta/blocks/soc-block/index.php
@@ -0,0 +1,7 @@
+<!-- Блок соцсетей -->
+<div class="soc-block">
+	<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-facebook" aria-hidden="true"></i></span>
+	<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-linkedin" aria-hidden="true"></i></span>	
+	<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-twitter" aria-hidden="true"></i></span>	
+	<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-vk" aria-hidden="true"></i></span>	
+</div>
